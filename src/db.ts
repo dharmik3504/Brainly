91143d5b9d94822bcfa06ae1ae197f8d350c34b9
dharmik3504 <heavynotebook@gmail.com>
2024-12-11 22:25:31 +0530

    init

---
+++ src/db.ts
@@ -0,0 +1,35 @@
+import mongoose, { Types } from 'mongoose';
+const {Schema,model} =mongoose
+
+const contentTypes = ['image', 'video', 'article', 'audio']; 
+const UserSchema=new Schema({
+    username:{type:String,require:true,unique:true},
+    password:{type:String,require:true}
+})
+
+const TagSchema=new Schema({
+    title:{type:String,require:true,unique:true}
+})
+
+const ContentSchema=new Schema({
+    link:{type:String,require:true},
+    type:{type:String,require:true,emum:contentTypes},
+    title:{type:String,require:true},
+    tag:[{type:Types.ObjectId,ref:"Tag"}],
+    userId:{type:Types.ObjectId,ref:"User",require:true}
+})
+
+const LinkSchema=new Schema({
+    hash:{type:String,require:true},
+    userId:{type:Types.ObjectId,ref:"User",require:true}
+
+})
+
+const User=model("User",UserSchema)
+const Content=model("Content",ContentSchema)
+const Tag=model("Tag",TagSchema)
+const Link=model("Link",LinkSchema)
+
+module.exports={
+    User,Content,Tag,Link
+}
