70be7de3336c57a8c1e63e08370890a3f7f49073
dharmik3504 <heavynotebook@gmail.com>
2024-12-12 12:01:24 +0530

    added zod and hashed the password

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,45 @@
-import express from "express"
-import {User} from './db'
-const app=express()
-app.use(express.json())
+import express from "express";
+import { User } from "./db";
+import z from "zod";
+import bcrypt from "bcrypt";
+const app = express();
+app.use(express.json());
 
-app.post("/api/v1/signup",async (req,res)=>{
-const {username,password} =req.body
+app.post("/api/v1/signup", async (req, res) => {
+  const requestBodyObj = z.object({
+    username: z.string().min(3).max(20),
+    password: z.string().min(3).max(20),
+  });
+  const isRequestBodyObjValid = requestBodyObj.safeParse(req.body);
+  if (isRequestBodyObjValid.success) {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 5);
+    try {
+      const data = await User.create({
+        username,
+        password: hashedPassword,
+      });
+      if (data) {
+        res
+          .status(200)
+          .json({ message: "user has created now you can login in" });
+      } else {
+        res.status(401).json({ message: "something went wrong" });
+      }
+    } catch (e: any) {
+      res.status(401).json({ message: e.message });
+    }
+  } else {
+    res.status(411).json({ isRequestBodyObjValid });
+  }
+});
+app.post("/api/v1/signin", async (req, res) => {});
+app.post("/api/v1/content", async (req, res) => {});
+app.get("/api/v1/content", async (req, res) => {});
 
-const data= await User.create({
-    username,
-    password
-})
-if(data){
-    res.status(200).json({message:"user has created now you can login in"})
-}
-else{
-    res.status(401).json({message:"something went wrong"})
-}
+app.delete("/api/v1/content", async (req, res) => {});
+app.post("/api/v1/brain/share", async (req, res) => {});
 
-})
-app.post("/api/v1/signin",(req,res)=>{
+app.get("/api/v1/brain/:shareLink", async (req, res) => {});
 
-})
-app.post("/api/v1/content",(req,res)=>{
-
-})
-app.get("/api/v1/content",(req,res)=>{
-
-})
-
-app.delete("/api/v1/content",(req,res)=>{
-
-})
-app.post("/api/v1/brain/share",(req,res)=>{
-
-})
-
-app.get("/api/v1/brain/:shareLink",(req,res)=>{
-
-})
-
-app.listen(3000)
+app.listen(3000);
